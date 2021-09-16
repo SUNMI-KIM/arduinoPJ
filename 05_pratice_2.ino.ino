f55c452084bcc748f7eef27d72170568413ba112
SUNMI <hariaus@kookmin.ac.kr>
2021-09-16 16:04:31 +0900

    modifie arduino and code

--- 05_pratice_2.ino.ino
+++ 05_pratice_2.ino.ino
@@ -9,15 +9,15 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   while (time + 1000 > millis()) {
-    digitalWrite(7, HIGH);
+    digitalWrite(7, LOW);
   }
   while (time + 2000 > millis()) {
-    digitalWrite(7, LOW);
-    delay(125);
     digitalWrite(7, HIGH);
+    delay(125);
+    digitalWrite(7, LOW);
     delay(125);
   }
   while(1) {
-    digitalWrite(7, LOW);
+    digitalWrite(7, HIGH);
 }
 }
